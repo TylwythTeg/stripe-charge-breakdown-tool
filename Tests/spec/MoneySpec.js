1c5a337699e1d316a66e9c8c6c33955380eb71eb
TylwythTeg <garciarobertoiv@yahoo.com>
2017-12-28 02:29:00 -0800

    Now using Jasmine to run tests on this tool little by little. I started here purely with Money objects. I already definitely made the code a little better, so excited to flesh this out and have a bunch of really comprehensive tests for this thing

---
+++ Tests/spec/MoneySpec.js
@@ -0,0 +1,37 @@
+
+
+describe ("Money Object or Module", function () {
+
+
+  it("converts itself to another currency, based on a given rate", function () {
+    var money = new Money(100, "USD");
+    expect(money.convertTo("GBP").amount.toString()).toEqual("127.88");
+  });
+
+  it("automatically rounds the amount passed in based on the currency", function () {
+    var money = new Money(100.764, "USD");
+    expect(money.amount.toString()).toEqual("100.76");
+  });
+
+  it("supports 0 decimal currencies", function () {
+    money = new Money(100.764, "JPY");
+    expect(money.amount.toString()).toEqual("101");
+  });
+
+
+  it("adds an interface for direct math using decimal.js (times, plus, minus)", function () {
+    var money = new Money(22.33, "USD");
+    var outcome = money.times(12.4347).amount.toString();
+    expect(outcome).toEqual("277.67");
+    outcome = money.minus(12.4347).amount.toString();
+    expect(outcome).toEqual("9.9");
+  });
+
+
+
+
+
+    
+
+
+});
