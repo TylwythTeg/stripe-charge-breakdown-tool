90be637ed24f4f590977dcff45e6fadf0da2648b
TylwythTeg <garciarobertoiv@yahoo.com>
2017-12-29 23:07:00 -0800

    Added tests for Pricing and merged Stripe Countries as apart of the pricing module since they were only containers  to hold their pricing model objects. Now you just Pricing.Model.fromCountry() to get pricing for a country

--- Tests/spec/PricingSpec.js
+++ Tests/spec/PricingSpec.js
@@ -1,25 +1,48 @@
 describe ("Pricing", function () {
 
 
+    it("Pricing object accepts string 'percent + fixed' and has a percent and fixed attribute (Decimal, Money)", function () {
+        var domesticPricing = new Pricing("2.9 + 0.30", "USD");
+        var pricing = domesticPricing;
 
-  it("constructs pricing", function () {
-
-    console.log(Pricing.Model);
-
-    var usModel = new Pricing.Model("US", {
-        domestic: "2.9 + 0.30",
-        international: "3.9 + 0.30",
-        fxPercent: 1,
-        currency: "USD"
+        expect(pricing.percent.toString()).toEqual("2.9");
+        expect(pricing.percentMultiplier.toString()).toEqual("0.029");
+        expect(pricing.fixed.amount.toString()).toEqual("0.3");
+        expect(pricing.currency).toEqual("USD");
     });
 
-    console.log(usModel);
+    describe ("Pricing Model", function () {
+        it("constructs pricing model for a country with domestic, international, converison pricing", function () {
 
-    console.log(usModel.domestic.toString());
-    console.log(usModel.international.toString());
-    console.log(usModel.toString());
+        var usModel = new Pricing.Model("US", {
+            domestic: "2.9 + 0.30",
+            international: "3.9 + 0.30",
+            fxPercent: 1,
+            currency: "USD"
+        });
 
-  });
+        expect(usModel.domestic.toString()).toEqual("2.9 + 0.3 USD");
+        expect(usModel.international.toString()).toEqual("3.9 + 0.3 USD");
+
+        expect(usModel.fxPercent.toString()).toEqual("1");
+        expect(usModel.fxMultiplier.toString()).toEqual("0.01");
+
+        });
+
+
+        it("sets a dictionary of each pricing model for each Stripe Country and can retrieve entry by country code", function () {
+
+            var pricingModel = Pricing.Model.fromCountry("US");
+
+            expect(pricingModel.domestic.toString()).toEqual("2.9 + 0.3 USD");
+            expect(pricingModel.international.toString()).toEqual("3.9 + 0.3 USD");
+
+            expect(pricingModel.fxPercent.toString()).toEqual("1");
+            expect(pricingModel.fxMultiplier.toString()).toEqual("0.01");
+
+        });
+    });
+
 
 
 
--- Tests/src/Pricing.js
+++ Tests/src/Pricing.js
@@ -5,6 +5,161 @@ var Pricing = (function () {
     /* In addition to fx percent and multiplier () */
     /* Will be used specifically for the countries */
     Pricing.Model = (function () {
+        var countries = {
+            "US": new PricingModel("US",{
+                domestic: "2.9 + 0.30",
+                international: "3.9 + 0.30",
+                fxPercent: 1,
+                currency: "USD"
+            }),
+            "AU": new PricingModel("AU",{
+                domestic: "1.75 + 0.30",
+                international: "2.9 + 0.30",
+                fxPercent: 2,
+                currency: "AUD"
+            }),
+            "AT": new PricingModel("AT", {
+                domestic: "1.4 + 0.25",
+                international: "2.9 + 0.25",
+                fxPercent: 2,
+                currency: "EUR"
+            }),
+            "BE": new PricingModel("BE", {
+                domestic: "1.4 + 0.25",
+                international: "2.9 + 0.25",
+                fxPercent: 2,
+                currency: "EUR"
+            }),
+            "BR": new PricingModel("BR", {
+                domestic: "3.99 + 0.50",
+                international: "3.99 + 0.50",
+                fxPercent: 2,
+                currency: "BRL"
+            }),
+            "CA": new PricingModel("CA", {
+                domestic: "2.9 + 0.30",
+                international: "3.5 + 0.30",
+                fxPercent: 2,
+                currency: "CAD"
+            }),
+            "DK": new PricingModel("DK", {
+                domestic: "1.4 + 1.80",
+                international: "2.9 + 1.80",
+                fxPercent: 2,
+                currency: "DKK"
+            }),
+            "FI": new PricingModel("FI", {
+                domestic: "1.4 + 0.25",
+                international: "2.9 + 0.25",
+                fxPercent: 2,
+                currency: "EUR"
+            }),
+            "FR": new PricingModel("FR", {
+                domestic: "1.4 + 0.25",
+                international: "2.9 + 0.25",
+                fxPercent: 2,
+                currency: "EUR"
+            }),
+            "DE": new PricingModel("DE", {
+                domestic: "1.4 + 0.25",
+                international: "2.9 + 0.25",
+                fxPercent: 2,
+                currency: "EUR"
+            }),
+            "HK": new PricingModel("HK", {
+                domestic: "3.4 + 2.35",
+                international: "3.4 + 2.35",
+                fxPercent: 2,
+                currency: "HKD"
+            }),
+            "IE": new PricingModel("IE", {
+                domestic: "1.4 + 0.25",
+                international: "2.9 + 0.25",
+                fxPercent: 2,
+                currency: "EUR"
+            }),
+            "JP": new PricingModel("JP", {
+                domestic: "3.6 + 0",
+                international: "3.6 + 0",
+                fxPercent: 2,
+                currency: "JPY"
+            }),
+            "LU": new PricingModel("LU", {
+                domestic: "1.4 + 0.25",
+                international: "2.9 + 0.25",
+                fxPercent: 2,
+                currency: "EUR"
+            }),
+            "NL": new PricingModel("NL", {
+                domestic: "1.4 + 0.25",
+                international: "2.9 + 0.25",
+                fxPercent: 2,
+                currency: "EUR"
+            }),
+            "NZ": new PricingModel("NZ", {
+                domestic: "2.9 + 0.3",
+                international: "2.9 + 0.3",
+                fxPercent: 2,
+                currency: "NZD"
+            }),
+            "NO": new PricingModel("NO", {
+                domestic: "2.4 + 2",
+                international: "2.9 + 2",
+                fxPercent: 2,
+                currency: "NOK"
+            }),
+            "MX": new PricingModel("MX", {
+                domestic: "3.6 + 3",
+                international: "3.6 + 3",
+                fxPercent: 2,
+                currency: "MXN"
+            }),
+            "SG": new PricingModel("SG", {
+                domestic: "3.4 + 0.5",
+                international: "3.4 + 0.5",
+                fxPercent: 2,
+                currency: "SGD"
+            }),
+            "ES": new PricingModel("ES", {
+                domestic: "1.4 + 0.25",
+                international: "2.9 + 0.25",
+                fxPercent: 2,
+                currency: "EUR"
+            }),
+            "SE": new PricingModel("SE", {
+                domestic: "1.4 + 1.8",
+                international: "2.9 + 1.8",
+                fxPercent: 2,
+                currency: "SEK"
+            }),
+            "CH": new PricingModel("CH", {
+                domestic: "2.9 + 0.30",
+                international: "2.9 + 0.30",
+                fxPercent: 2,
+                currency: "CHF"
+            }),
+            "GB": new PricingModel("GB", {
+                domestic: "1.4 + 0.20",
+                international: "2.9 + 0.20",
+                fxPercent: 2,
+                currency: "GBP"
+            }),
+            "IT": new PricingModel("IT", {
+                domestic: "1.4 + 0.25",
+                international: "2.9 + 0.25",
+                fxPercent: 2,
+                currency: "EUR"
+            }),
+            "PT": new PricingModel("PT", {
+                domestic: "1.4 + 0.25",
+                international: "2.9 + 0.25",
+                fxPercent: 2,
+                currency: "EUR"
+            }),
+
+        };
+
+
         function PricingModel(country, pricing) {
             this.currency = pricing.currency;
             this.fxPercent = new Decimal(pricing.fxPercent);
@@ -15,6 +170,9 @@ var Pricing = (function () {
         }
         PricingModel.prototype.toString = function() {
             return JSON.stringify(this);
+        };
+        PricingModel.fromCountry = function(code) {
+            return countries[code];
         };
         return PricingModel;
     })();
